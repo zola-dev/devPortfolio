068baa8174d0067b72e87fcaef6ca66defde1757
Milos <miloslazoviczola@gmail.com>
2026-02-17 22:37:18 +0100

    randomDelay to 7-15sec

--- src/app/core/services/update-app.ts
+++ src/app/core/services/update-app.ts
@@ -53,7 +53,8 @@ export class UpdateApp {
   }
 
   private async showUpdatePrompt(versionInfo: any) {
-    const randomDelay = Math.floor(Math.random() * (7 - 2 + 1) + 2) * 1000;
+    // const randomDelay = Math.floor(Math.random() * (7 - 2 + 1) + 2) * 1000;
+    const randomDelay = Math.floor(Math.random() * (15 - 5 + 1) + 7) * 1000;
 
     const commit = versionInfo.commit;
     const commitMessage = commit?.message || 'New features and improvements';
